2ecc1538f20e5bcbf91c7c1992606aad7f1b770c
Guy Mograbi <guy.mograbi@gmail.com>
2018-05-16 16:02:25 -0700

    add getMerged to list merged branches to some target branch. add getUpstream to get the upstream remote branch for given branch.

--- index.js
+++ index.js
@@ -10,13 +10,23 @@ exports.isClean = () => {
   return shell.exec(`git status`, {silent: true}).toString().includes('nothing to commit');
 }
 
+// gets which remote is being tracked by this branch. empty string means there's no remote branch being tracked
+exports.getUpstream = (branch) => {
+  return shell.exec(`git rev-parse --abbrev-ref ${branch}@{upstream}`, {silent: true}).toString();
+}
+
+// list all branches that were merged to branch X
+exports.getMergedBranches = (targetBranch) {
+  return exports.all(shell.exec(`git branch --merged ${targetBranch}`, {silent: true}).toString().split('\n').filter(f=>f!=''));
+}
+
 exports.current = () => {
   return allLocalBranches.find(b => b[0] === '*').split('*')[1].trim();
 }
 
 exports.other = () => allLocalBranches.filter(l => l[0] !== '*').map((n) => n.trim());
 
-exports.all = () => allLocalBranches.map(b => {
+exports.all = (branches=allLocalBranches) => branches.map(b => {
   if (b[0] === '*') {
     return b.slice(1).trim();
   } else {
